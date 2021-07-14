28cc8138f9bbefbc1a6f7ce1053af917152c5ff9
Richa Bharti <richa.bharti1@gmail.com>
2021-07-14 15:32:43 +0530

    feat: add line in background script

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -3,3 +3,4 @@ console.log('second attempt to change log');
 console.log('third attempt to change log');
 console.log('forth attempt to change log');
 
+console.log('Background script : fifth attempt to change log');
